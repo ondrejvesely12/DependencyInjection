91977026e79213ace7ae89422bb9c5f0abe13698
Ondrej Vesely <ondrej.vesely@me.com>
2021-09-16 05:52:34 +0200

    InjectedObservableObject

--- Sources/DependencyInjection/PropertyWrapper/InjectedObservableObject.swift
+++ Sources/DependencyInjection/PropertyWrapper/InjectedObservableObject.swift
@@ -1,5 +1,5 @@
 //
-//  ObservableObjectInjected.swift
+//  InjectedObservableObject.swift
 //
 //  Created by Ondřej Veselý on 16.09.2021.
 //
@@ -9,14 +9,14 @@ import SwiftUI
 
 /// Immediate injection property wrapper for SwiftUI ObservableObjects.
 /// This wrapper is meant for use in SwiftUI Views and exposes
-/// bindable objects similar to that of SwiftUI @observedObject and @environmentObject.
+/// bindable objects similar to that of SwiftUI @ObservedObject and @EnvironmentObject.
 ///
 /// Dependent service must be of type ObservableObject. Updating object state will trigger view update.
 ///
 /// Wrapped dependent service is resolved immediately using DependencyContainer upon struct initialization.
 ///
 @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-@propertyWrapper struct ObservableObjectInjected<Service>: DynamicProperty where Service: ObservableObject {
+@propertyWrapper public struct InjectedObservableObject<Service>: DynamicProperty where Service: ObservableObject {
     // MARK: - Properties
 
     // MARK: Private
@@ -25,22 +25,22 @@ import SwiftUI
 
     // MARK: Public
 
-    var wrappedValue: Service {
+    public var wrappedValue: Service {
         get { return service }
         mutating set { service = newValue }
     }
 
-    var projectedValue: ObservedObject<Service>.Wrapper {
+    public var projectedValue: ObservedObject<Service>.Wrapper {
         return $service
     }
 
     // MARK: - Initialization
 
-    init(container: DependencyContainerProtocol) {
+    public init(container: DependencyContainerProtocol) {
         service = container.resolve()
     }
 
-    init(name: String, container: DependencyContainerProtocol) {
+    public init(name: String, container: DependencyContainerProtocol) {
         service = container.resolve(name: name)
     }
 }
